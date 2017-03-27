9bfc5bd251787ce23261cb0a65169d44f61f8fd0
hkarambizi <hkarambizi@gmail.com>
2017-03-27 15:39:08 -0400

    updated index controller name, edit css style, added more console.logs for mongoose connection, and added links in layout.hbs.

--- workbox-app/server.js
+++ workbox-app/server.js
@@ -7,9 +7,10 @@ var hbs = require('hbs')
 var mongoose = require('mongoose');
 pry = require('pryjs');
 
-var usersController = require('./controllers/users.js');
-var sessionsController = require('./controllers/sessions.js');
-
+var usersController = require('./controllers/userController.js');
+var contactController = require('./controllers/contactController.js')
+var sessionsController = require('./controllers/sessionController.js');
+var indexController = require('./controllers/indexController.js')
 var app = express();
 
 //I used 'dotenv' to create an environment variable (stored in .env file) with the same name as the deployment database that calls the local host database as well.
@@ -29,6 +30,7 @@ app.use(session({
   saveUninitialized: false
 }));
 
+app.use('/', indexController);
 app.use('/users', userController);
 app.use('/contacts', contactController);
 app.use('/sessions', sessionController);
@@ -36,17 +38,22 @@ app.use('/sessions', sessionController);
 // I assigned the variable 'db' to my database connection
 var db = mongoose.connection;
 
-// Will log an error if db can't connect to MongoDB
-db.on('error', function(err){
-  console.log(err);
-});
-
-// Will log "database has been connected" if it successfully connects.
+// CONNECTION EVENTS
 db.once('open', function() {
   console.log("Yah bish! database has been connected!");
 });
-
+db.once('close', function() {
+  console.log("Closed mongoose.");
+});
+db.on('connected', function() {
+  console.log('Yah bish! database has been connected. Mongoose connected to ' + db.host + ':' + db.port + '/' + db.name);
+});
+db.on('error', function(err) {
+  console.log('Mongoose connection error: ' + err);
+});
+db.on('disconnected', function() {
+  console.log('Mongoose disconnected');
+});
 app.listen(4000, function(){
   console.log("app listening on muh fuh port 3000");
 });
-
