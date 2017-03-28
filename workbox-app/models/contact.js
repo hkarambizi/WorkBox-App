3d4390b962b00aef55cb883ea66a94b9b476d149
hkarambizi <hkarambizi@gmail.com>
2017-03-28 17:25:16 -0400

    Moved app to outer root folder

--- workbox-app/models/contact.js
+++
@@ -1,7 +0,0 @@
-// create the below bridge to import(require) and export(module.export) data
-//from schema.js
-var Schema = require('../db/schema');
-var mongoose = require('mongoose');
-
-var Contact = Schema.Contact;
-module.exports = Contact;
--- workbox-app/models/user.js
+++
@@ -1,7 +0,0 @@
-// create the below bridge to import(require) and export(module.export) data
-//from schema.js
-var Schema = require('../db/schema');
-var mongoose = require('mongoose');
-
-var User = Schema.User;
-module.exports = User;
